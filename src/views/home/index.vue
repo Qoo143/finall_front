6f0eb686f7dd742a3f494c60cfb59decbccdc137
Qoo143 <wayne676803@gmail.com>
2025-05-02 15:15:43 +0800

    完成vue動畫

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,63 @@
 <template>
-  <div><Headpart class="header" /></div>
+  <div class="wrapper">
+    <button @click="isShow = !isShow">test</button>
+    <transition name="fade">
+      <div v-if="isShow" class="testP"></div>
+    </transition>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Headpart from "@/components/Headpart.vue";
+import { ref } from "vue";
+const isShow = ref(false);
 </script>
 
 <style scoped lang="scss">
-.layout {
+.wrapper {
   width: 100%;
-  height: 100vh;
-  background-color: $bg-3;
-
-  .header {
-    width: 100%;
-    height: 60px;
+  height: 100%;
+  background-color: $primary-b-ll;
+  display: flex;
+  position: relative;
+  justify-content: center;
+  button {
+    margin-top: 50px;
+    padding: 10px 10px 10px 10px;
+    font-size: 16px;
+    height: 32px;
+    line-height: 0.8;
   }
+  .testP {
+    width: 100px;
+    height: 100px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+
+    border: 2px solid red;
+  }
+}
+</style>
+<style lang="scss">
+//定義vue-animation
+.fade-enter-from {
+  opacity: 0;
+  transform: translate(-10%, -10%);
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-enter-active {
+  transition: all 0.8s ease;
+}
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translate(-10%, -10%);
+}
+.fade-leave-active {
+  transition: all 0.8s ease;
 }
 </style>
